f199ed5193e70e054e94c0dfe443e4869a31dfac
DanPaul12 <dan.paul.schechter@gmail.com>
2024-12-08 15:55:02 -0800

    hw65

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from web_socket_server import WebSocketServer, socketio, app
-from flask import render_template
+from flask import render_template, request, jsonify
 import json
 
 app = WebSocketServer().create_app()
-#messages = []
+
 message_storage = {}
 
 @socketio.on('connect')
@@ -15,20 +15,29 @@ def handle_disconnect():
     print('Client disconnected')
 
 @socketio.on('message')
-def handle_message(response):
-    #print(f'Received message: {message}')
-    #messages.append(message)
-    res = json.loads(response)
-    user = res['user']
-    message = res['message']
+def handle_message(data):
+    print(data)
+    user = data.get('username')
+    message = data.get('message')
+    
+    if not user or not message:
+        return  
+    
     if user not in message_storage:
-        message_storage[user]=[]
+        message_storage[user] = []
     message_storage[user].append(message)
-    socketio.emit('message', message)
+    
+    print(f"Message from {user}: {message}")
+    
+    socketio.emit('message', {'username': user, 'message': message})
+
 
 @socketio.on('get_user_messages')
 def handle_get_user_messages(data):
-    socketio.emit('get_user_messages', message_storage)
+    user = data.get('user')
+    user_messages = message_storage.get(user, [])
+    socketio.emit('get_user_messages', {'user': user, 'messages': user_messages})
+
 
 @app.route('/')
 def index():
